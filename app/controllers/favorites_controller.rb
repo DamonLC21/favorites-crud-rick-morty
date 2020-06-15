08519813edb1a182b3b4ed7279cfc55bfb515a5a
DamonLC21 <chiversdamon@gmail.com>
2020-06-15 12:48:18 -0600

    backend for rick and morty api crud

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,51 @@
+class FavoritesController < ApplicationController
+  before_action :set_favorite, only: [:show, :update, :destroy]
+
+
+  def index
+    @favorites = Favorite.all
+
+    render json: @favorites
+  end
+
+
+  def show
+    render json: @favorite
+  end
+
+
+  def create
+    @favorite = Favorite.new(favorite_params)
+
+    if @favorite.save
+      render json: @favorite, status: :created, location: @favorite
+    else
+      render json: @favorite.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  def update
+    if @favorite.update(favorite_params)
+      render json: @favorite
+    else
+      render json: @favorite.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    @favorite.destroy
+  end
+
+  private
+  
+    def set_favorite
+      @favorite = Favorite.find(params[:id])
+    end
+
+  
+    def favorite_params
+      params.require(:favorite).permit(:api_id, :name)
+    end
+end
